2363c781de151149ab4067ee70194c54a7f65f7c
Noelby13 <Hnoelreyes25@gmail.com>
2023-05-26 01:00:04 -0600

    se agrega se agrega capa de negocios, se realizan cambios sugeridos en clase en CtrlFrmRegion, adema se optmizan la creación de algunos objetos

--- datos/Dt_Region.py
+++ datos/Dt_Region.py
@@ -4,17 +4,18 @@ from datos.conexion import Conexion
 
 
 class Dt_Region:
+    _con = None
+    _cursor = None
 
     def __init__(self):
-        self._con = None
-        self._cursor = None
-        #self._cursor = Conexion.getCursor()
+        pass
+
 
     def listarRegion(self):
         listaRegion = []
         try:
             self._con = Conexion.getConnection()
-            self._cursor = self._con.cursor()
+            self._cursor = Conexion.getCursor()
             sql = "SELECT * FROM Seguridad.regions;"
             self._cursor.execute(sql)
             registros = self._cursor.fetchall()
@@ -23,63 +24,31 @@ class Dt_Region:
                 nombre = r['region_name']
                 region = Region(id, nombre)
                 listaRegion.append(region)
-            self._cursor.close()
             return listaRegion
         except Exception as e:
-            print("Error en la consulta", e)
-            if self._con.open:
-                self._con.close()
-                self._cursor.close()
-                print("MySQL connection was closed")
+            print("Datos: Error listarRegion", e)
+        finally:
+            Conexion.closeCursor()
+            Conexion.closeConnection()
 
-    def buscarRegion(self, Region_param):
+    def filtrarRegion(self, region_param):
         listaRegion = []
         try:
             self._con = Conexion.getConnection()
-            self._cursor = self._con.cursor()
-
-            sql = f"SELECT * FROM Seguridad.regions WHERE region_name LIKE '%{Region_param.region_name}%';"
-            self._cursor.execute(sql)
-
+            self._cursor = Conexion.getCursor()
+            sql = f"SELECT * FROM Seguridad.regions WHERE region_name LIKE '%{region_param.region_name}%';"
+            num= self._cursor.execute(sql)
+            print(f"Hola soy el resultado del executre : {num}")
             registros = self._cursor.fetchall()
             for r in registros:
                 id = r['region_id']
                 nombre = r['region_name']
                 region = Region(id, nombre)
                 listaRegion.append(region)
-                print(f"Region: {region}")
-            self._cursor.close()
             return listaRegion
-        except Exception as e:
-            print("Error en la consulta", e)
-            if self._con.open:
-                self._con.close()
-                self._cursor.close()
-                print("MySQL connection was closed")
 
-    def buscar(self, region_param):
-        listaRegion = []
-        try:
-            con = Conexion.getConnection()
-            cursor = Conexion.getCursor()
-            print(con.open)
-            if cursor:
-                print("Estoy abierto")
-            else:
-                print("Estoy cerrado")
-            sql = f"SELECT * FROM Seguridad.regions WHERE region_name LIKE '%{region_param.region_name}%';"
-            num= cursor.execute(sql)
-            print(f"Hola soy el resultado del executre : {num}")
-            registros = cursor.fetchall()
-            for r in registros:
-                id = r['region_id']
-                nombre = r['region_name']
-                region = Region(id, nombre)
-                listaRegion.append(region)
-                print(f"Region: {region}")
-            return listaRegion
         except Exception as e:
-            print("Error en la consulta", e)
+            print("Datos: Error en listaRegion()", e)
         finally:
             Conexion.closeCursor()
             Conexion.closeConnection()
@@ -87,47 +56,71 @@ class Dt_Region:
     def insertarRegion(self, region_param):
         resultado = False
         try:
-            con = Conexion.getConnection()
-            cursor = Conexion.getCursor()
+            self._con = Conexion.getConnection()
+            self._cursor = Conexion.getCursor()
             sql = f"INSERT INTO Seguridad.regions(region_name) VALUES ('{region_param.region_name}');"
-            if cursor.execute(sql) > 0:
+            if self._cursor.execute(sql) > 0:
                 resultado = True
-            con.commit()
+            self._con.commit()
+            return resultado
+
         except Exception as e:
-            print(f"Error en la insercion: {e}")
+            print(f"Datos: Error en insertarRegion() {e}")
         finally:
             Conexion.closeCursor()
             Conexion.closeConnection()
-            return resultado
 
     def actualizarRegion(self, region_param):
         resultado = False
         try:
-            con = Conexion.getConnection()
-            cursor = Conexion.getCursor()
+            self._con = Conexion.getConnection()
+            self._cursor = Conexion.getCursor()
             sql = f"UPDATE Seguridad.regions SET region_name = '{region_param.region_name}' WHERE region_id = {region_param.region_id};"
-            if cursor.execute(sql) > 0:
+            if self._cursor.execute(sql) > 0:
                 resultado = True
-            con.commit()
+            self._con.commit()
+            return resultado
+
         except Exception as e:
-            print(f"Error en la actualizacion: {e}")
+            print(f"Datos: Error en actualizarRegion() {e}")
         finally:
             Conexion.closeCursor()
             Conexion.closeConnection()
-            return resultado
+
 
     def eliminarRegion(self, region_param):
         resultado = False
         try:
-            con = Conexion.getConnection()
-            cursor = Conexion.getCursor()
+            self._con = Conexion.getConnection()
+            self._cursor = Conexion.getCursor()
             sql = f"DELETE FROM Seguridad.regions WHERE region_id = {region_param.region_id};"
-            if cursor.execute(sql) > 0:
+            if self._cursor.execute(sql) > 0:
                 resultado = True
-            con.commit()
+            self._con.commit()
+            return resultado
+
         except Exception as e:
-            print(f"Error en la eliminacion: {e}")
+            print(f"Datos: Error en eliminarRegion() {e}")
         finally:
             Conexion.closeCursor()
             Conexion.closeConnection()
+
+    # Retorta True si existe esa región
+    def existeRegion(self, region_param):
+        resultado = False
+        try:
+            self._con = Conexion.getConnection()
+            self._cursor = Conexion.getCursor()
+            sql = f"SELECT * FROM Seguridad.regions WHERE region_name = '{region_param.region_name}';"
+            self._cursor.execute(sql)
+            if self._cursor.rowcount > 0:
+                resultado = True
+
             return resultado
+
+        except Exception as e:
+            print(f"Datos: Error en ExisteRegion() {e}")
+        finally:
+            Conexion.closeCursor()
+            Conexion.closeConnection()
+
