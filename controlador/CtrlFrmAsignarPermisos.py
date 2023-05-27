108ae2e69bc68e716aaa545fdfad9cb5275b9bae
Emanuel <emanuel.leite10@outlook.com>
2023-05-26 20:28:31 -0600

    Agregando llenado de comboBox y de Tabla en Permisos y Asignar Rol

--- controlador/CtrlFrmAsignarPermisos.py
+++ controlador/CtrlFrmAsignarPermisos.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 from vistas.FrmAsignarPermisos import Ui_FrmAsignarPermisos
+from datos.Dt_tbl_rol import Dt_tbl_rol
 
 
 class CtrlFrmAsignarPermisos(QtWidgets.QMainWindow):
@@ -9,7 +10,16 @@ class CtrlFrmAsignarPermisos(QtWidgets.QMainWindow):
         self.ui = Ui_FrmAsignarPermisos()
         self.ui.setupUi(self)
         self.initControlGui()
+    dtrol = Dt_tbl_rol()
 
     def initControlGui(self):
-        #self.ui.btn_entrar.clicked.connect(self.validarCredenciales)
-        print()
+        self.cargarCbxRol()
+        self.limpiarCampos()
+
+    def limpiarCampos(self):
+        self.ui.cbx_rol.setCurrentIndex(-1)
+
+    def cargarCbxRol(self):
+        self.listaRol = self.dtrol.llenarCbxRol()
+        for row in self.listaRol:
+            self.ui.cbx_rol.addItem(row.rol, row.id_rol)
